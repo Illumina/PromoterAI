eb07d07c822f8a7ae82cce5768191cc29b3f6a8c
Jaganathan <kjaganatha@usfc-prd-brln02.cm.cluster>
2025-06-19 22:39:25 -0700

    1.0rc4

--- setup.py
+++ setup.py
@@ -4,19 +4,20 @@ import io
 setup(
     name='promoterai',
     packages=['promoterai'],
-    version='1.0rc3',
+    version='1.0rc4',
     description='Predict the impact of promoter variants on gene expression',
     long_description=io.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Illumina/PromoterAI',
     license='PolyForm-Strict-1.0.0',
-    python_requires='>=3.9',
+    python_requires='>=3.8',
     install_requires=[
-        'numpy>=1.26.2',
-        'pandas>=2.1.4',
-        'pyfaidx>=0.8.1.1',
-        'pybigwig>=0.3.22',
-        'tensorflow>=2.13,<2.16'
+        'numpy>=1.20',
+        'pandas>=2.0',
+        'pyfaidx>=0.8',
+        'pybigwig>=0.3',
+        'tensorflow>=2.13,<2.16',
+        'keras>=2.13,<3.0'
     ],
     entry_points={'console_scripts': ['promoterai=promoterai.score:main']},
     author='Kishore Jaganathan',
